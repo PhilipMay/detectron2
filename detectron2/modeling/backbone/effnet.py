cf1dd964584c4e044b9a70429da0afef43a56143
PhilipMay <eniak.info@gmail.com>
2020-04-26 18:59:21 +0200

    eff b6

--- detectron2/modeling/backbone/effnet.py
+++ detectron2/modeling/backbone/effnet.py
@@ -25,7 +25,7 @@ class EffNet(Backbone):
     def __init__(self):
         super(EffNet, self).__init__()
         #logger.info("EfficientNet.__init__")
-        self._eff_model = EfficientNet.from_pretrained("efficientnet-b6", advprop=True)
+        self._eff_model = EfficientNet.from_pretrained("efficientnet-b4", advprop=True)
         #for idx, block in enumerate(self._eff_model._blocks):
             #logger.info("##################################")
             #logger.info("EfficientNet extract_features: {} - {}".format(idx, block))
